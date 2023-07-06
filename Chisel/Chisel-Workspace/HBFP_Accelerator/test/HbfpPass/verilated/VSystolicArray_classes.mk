0345d6e61ba3969daf612413c1bd186982b0f96f
Vrajesh Surendrakumar Patel <vpatel@iccluster109.iccluster.epfl.ch>
2023-07-06 14:55:45 +0000

    trash tests

--- Chisel/Chisel-Workspace/HBFP_Accelerator/test/HbfpPass/verilated/VSystolicArray_classes.mk
+++ Chisel/Chisel-Workspace/HBFP_Accelerator/test/HbfpPass/verilated/VSystolicArray_classes.mk
@@ -10,7 +10,7 @@ VM_C11 = 0
 # Coverage output mode?  0/1 (from --coverage)
 VM_COVERAGE = 1
 # Parallel builds?  0/1 (from --output-split)
-VM_PARALLEL_BUILDS = 0
+VM_PARALLEL_BUILDS = 1
 # Threaded output mode?  0/1/N threads (from --threads)
 VM_THREADS = 0
 # Tracing output mode?  0/1 (from --trace/--trace-fst)
@@ -24,19 +24,31 @@ VM_TRACE_FST_WRITER_THREAD = 0
 # Generated module classes, fast-path, compile with highest optimization
 VM_CLASSES_FAST += \
 	VSystolicArray \
+	VSystolicArray__1 \
+	VSystolicArray__2 \
+	VSystolicArray__3 \
+	VSystolicArray_PE \
+	VSystolicArray_PE__1 \
+	VSystolicArray_PE__2 \
 
 # Generated module classes, non-fast-path, compile with low/medium optimization
 VM_CLASSES_SLOW += \
 	VSystolicArray__Slow \
+	VSystolicArray__1__Slow \
+	VSystolicArray_PE__Slow \
 
 # Generated support classes, fast-path, compile with highest optimization
 VM_SUPPORT_FAST += \
 	VSystolicArray__Trace \
+	VSystolicArray__Trace__1 \
 
 # Generated support classes, non-fast-path, compile with low/medium optimization
 VM_SUPPORT_SLOW += \
 	VSystolicArray__Syms \
 	VSystolicArray__Trace__Slow \
+	VSystolicArray__Trace__1__Slow \
+	VSystolicArray__Trace__2__Slow \
+	VSystolicArray__Trace__3__Slow \
 
 # Global classes, need linked once per executable, fast-path, compile with highest optimization
 VM_GLOBAL_FAST += \
